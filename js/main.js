96a8cf703291d590d33a5c947dfcc01c3ba3e75f
Marisol Rodriguez <rodriguezsmarisol@gmail.com>
2022-07-26 21:49:35 -0300

    Correcciones

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-/*  
-    let imagenes=[
-        {  
-        id:1,
-        src:"https://pbs.twimg.com/media/FYjqBO8WAAEwljL?format=jpg&name=small",
-        },
-        { 
-        id:2,
-        src:"https://pbs.twimg.com/media/FYl74raXoAE4TbC?format=jpg&name=900x900",
-        },  
-        {
-        id:3,
-        src:"https://pbs.twimg.com/media/FYl74raXoAE4TbC?format=jpg&name=900x900",
-        }  
-    ]
-
-    console.log(imagenes.length);
-
-    let q = imagenes.length;
-    let numAleatorio=Math.round(Math.random()*(q-1))
-
-    console.log(numAleatorio);
-    console.log(imagenes.numAleatorio);
-
-    function imagenAleatoria (){  
-        let result = document.getElementById("result").innerHTML =
-        `<img src="${imagenes.numAleatorio}"></img>`
-    }   
-
-
-    imagenAleatoria() */
-
 function imagenAleatoria(){
   
         let imagenes = [
